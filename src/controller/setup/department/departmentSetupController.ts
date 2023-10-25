b7f7a8e1766c507da0a0f5e2e4567136f4de3f96
itaminul <itaminul@gmail.com>
2023-10-26 00:26:32 +0600

    department modify

--- src/controller/setup/department/departmentSetupController.ts
+++ src/controller/setup/department/departmentSetupController.ts
@@ -2,9 +2,39 @@ import { Request, Response } from "express";
 import { DepartmentSetupService } from "../../../services/setup/departmentSetupService";
 const departmentSetupService = new  DepartmentSetupService();
 export class DepartmentSetupController {
-  getDepartmentData = async(req: Request, res: Response): Promise<void> => {
-    const results = await departmentSetupService.getDepartmentSetupData();
-    res.json({ message: results});
+  getAll = async(req: Request, res: Response): Promise<void> => {
+    try {
+      const results = await departmentSetupService.getAll();
+      res.json({ message: results});
+    } catch (error) {
+      res.json({ message: error});      
+    }
   }
 
+  create = async(req: Request, res: Response): Promise<void> => {
+    try {
+      const results = await departmentSetupService.create();
+      res.json({ message: results});
+    } catch (error) {
+      res.json({ message: error});
+    }
+  }
+
+  update = async(req: Request, res: Response): Promise<void> => {
+    try {
+      const results = await departmentSetupService.update();
+      res.json({ message: results});
+    } catch (error) {
+      res.json({ message: error});
+    }
+  }
+
+  deleteDepartment = async(req: Request, res: Response): Promise<void> => {
+    try {
+      const results = await departmentSetupService.deleteDepartment();
+      res.json({ message: results});
+    } catch (error) {
+      res.json({ message: error});
+    }
+  }
 }
