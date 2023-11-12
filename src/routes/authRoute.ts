70ab97433f15cb9642de8526b92f420323cc5e31
Aminul PHP <aminul@atilimited.net>
2023-11-12 18:36:43 +0600

    user registration

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import { Authentication } from '../controller/auth/authController';
 const authentication = new Authentication();
 router.route('/').get(authentication.userLogin);
+router.route('/register').post(authentication.userLogin)
 
 export default router;
