e39e0c16b9c423824c4a81a18403ca9b8c865c0c
Aminul PHP <aminul@atilimited.net>
2023-11-20 19:02:14 +0600

    create employee informaton

--- src/controller/employee/employeeController.ts
+++ src/controller/employee/employeeController.ts
@@ -1,7 +1,9 @@
-import { NextFunction } from "express";
-import { handlePrismaError } from "../middleware/prismaErrorHandler";
-import { sendErrorResponse, sendSuccessResponse } from "../middleware/resposeMiddleware";
-import { EmployeeService } from "../services/employeeService";
+import { NextFunction, Request, Response } from "express";
+import { EmployeeService } from "../../services/employeeService";
+import { sendErrorResponse, sendSuccessResponse } from "../../middleware/resposeMiddleware";
+import { handlePrismaError } from "../../middleware/prismaErrorHandler";
+
+
 const empService = new EmployeeService();
 export class EmployeeController {
   getAll = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -15,9 +17,8 @@ export class EmployeeController {
 
   create = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const results = await empService.create(req, res);
-      // console.log("depar controller", results);
-      sendSuccessResponse(200, results, res);
+      const results = await empService.create(req, res, next);
+     return results;
     } catch (error) {
       sendErrorResponse(500, res, next);
     }
---
+++ src/controller/employee/employeeInformationType.ts
@@ -0,0 +1,13 @@
+export interface  EmployeeInformationType {
+  firstName: string,
+  middleName: string,
+  lastName: string,
+  fullName: string,
+  phone: string,
+  mobileOne: string,
+  mobileTwo: string,
+  emergencyMobile: string,
+  officeEmail: string,
+  personalEmail: string,
+  presentAddress: string
+}
