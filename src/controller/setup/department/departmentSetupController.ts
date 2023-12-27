9db2663b786fabe86791f5fce6a14f1d59acdb0e
itaminul <itaminul@gmail.com>
2023-12-27 15:26:11 +0600

    department validation

--- src/controller/setup/department/departmentSetupController.ts
+++ src/controller/setup/department/departmentSetupController.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
 import { DepartmentSetupService } from "../../../services/setup/departmentSetupService";
 import { sendErrorResponse, sendSuccessResponse } from "../../../middleware/resposeMiddleware";
 import { handlePrismaError } from "../../../middleware/prismaErrorHandler";
+
 const departmentSetupService = new  DepartmentSetupService();
 export class DepartmentSetupController {
   getAll = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -13,7 +15,13 @@ export class DepartmentSetupController {
     }
   }
 
-  create = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+  create = async(req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+      }
+
     try {
       const results = await departmentSetupService.create(req, res);
       // console.log("depar controller", results);
@@ -24,12 +32,17 @@ export class DepartmentSetupController {
   }
 
   update = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array()});
+    }
     try {     
       const results = await departmentSetupService.update(req, res);
       sendSuccessResponse(200, results, res);
     } catch (error) {
-       const errorMessage = handlePrismaError(error, req, res, next);
-    res.status(400).json({ success: false, error: errorMessage });
+      next();
+      //  const errorMessage = handlePrismaError(error, req, res, next);
+    //res.status(500).json({ error: error.message });
       // sendErrorResponse(500, res, next);
     }
   }
