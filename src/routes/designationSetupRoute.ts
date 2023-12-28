48e78dbc2192b4161b897126718f3baf0df49509
Aminul PHP <aminul@atilimited.net>
2023-12-28 19:03:12 +0600

    designation validation

--- src/routes/designationSetupRoute.ts
+++ src/routes/designationSetupRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { DesignationSetupController } from '../controller/setup/designation/designationSetupController';
+import { designationValidator } from '../validation/designationValidation';
 const router = express.Router();
 const designationSetupController = new DesignationSetupController();
-router.route('/').get(designationSetupController.getAll);
-router.route('/').post(designationSetupController.create);
-router.route('/:id').patch(designationSetupController.update);
-router.route('/').delete(designationSetupController.deleteDepartment);
+router.get('/', designationSetupController.getAll);
+router.post('/', designationValidator(), designationSetupController.create);
+router.patch('/:id', designationValidator(), designationSetupController.update);
+router.delete('/',designationSetupController.deleteDepartment);
 export default router;
