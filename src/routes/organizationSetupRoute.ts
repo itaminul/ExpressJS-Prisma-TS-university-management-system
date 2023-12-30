9d1cad68e925d19aeea20bbc3e3f76c18644519b
Aminul PHP <aminul@atilimited.net>
2023-12-30 18:07:27 +0600

    organization validation

--- src/routes/organizationSetupRoute.ts
+++ src/routes/organizationSetupRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { OrganizationSetupController } from '../controller/setup/organization/organizationSetupController';
+import { organizationCreateValidator, organizationUpdateValidator } from '../validation/organizationValidation';
 const router = express.Router();
 const organizationSetupController = new OrganizationSetupController()
 router.route('/').get(organizationSetupController.getAll);
-router.route('/').post(organizationSetupController.create);
-router.route('/:id').patch(organizationSetupController.update);
+router.post('/', organizationCreateValidator(), organizationSetupController.create);
+router.patch('/:id', organizationUpdateValidator(), organizationSetupController.update);
 router.route('/').delete(organizationSetupController.deleteOrganization);
 export default router;
