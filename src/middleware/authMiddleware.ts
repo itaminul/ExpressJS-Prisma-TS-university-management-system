09a92dcd037759652bee9d4479d13beadd8fdadb
Aminul PHP <aminul@atilimited.net>
2024-01-02 18:13:10 +0600

    authentication

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,25 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-
-const secretKey = 'your_secret_key_here'; // Replace with your secret key
-
-interface AuthRequest extends Request {
-  user?: JwtPayload; // Extend the Express Request type to include user information
-}
-
-export function authenticateToken(req: AuthRequest, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  console.log('token', token);
+import jwt from 'jsonwebtoken';
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1]; 
+  console.log("token", token);
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Access denied. Token is missing.' });
   }
-
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-    req.user = user as JwtPayload; // Attach the user object to the request
+  try {
+    const decoded = jwt.verify(token, 'JWT_SECRET') as { userId: string }; 
+    req.user = decoded.userId; 
     next();
-  });
-}
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token.' });
+  }
+};
+
