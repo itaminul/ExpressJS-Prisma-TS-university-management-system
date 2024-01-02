7ce978c3457be321c4f1d880ddac59609bc083f9
Aminul PHP <aminul@atilimited.net>
2024-01-02 15:41:38 +0600

    employee route modify

--- src/routes/employeeRoute.ts
+++ src/routes/employeeRoute.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { EmployeeController } from '../controller/employee/employeeController';
+import { employeeCreateValidation, employeeUpdateValidation } from '../validation/employeeInfoValidation';
 const employeeController = new EmployeeController();
 const router = express.Router();
-router.route('/').get(employeeController.getAll);
-router.route('/').post(employeeController.create);
-router.route('/:id').patch(employeeController.update);
+router.get('/',employeeController.getAll);
+router.post('/', employeeCreateValidation(),employeeController.create);
+router.patch('/:id', employeeUpdateValidation(),employeeController.update);
 export default router;
--- src/validation/employeeInfoValidation.ts
+++ src/validation/employeeInfoValidation.ts
@@ -18,3 +18,22 @@ export const employeeCreateValidation = (): ValidationChain[] => {
   ]
 
 }
+
+export const employeeUpdateValidation = (): ValidationChain[] => {
+  return[
+    body('firstName','First name max length 120').exists().isEmpty().isLength({ max: 150}),
+    body('phone', 'Phone number not null and not greater then 13 character').exists().notEmpty().isLength({ max: 13}),
+    body('middleName', 'Middle name not null and max length 50').notEmpty().isLength({ max: 50}),
+    body('lastName', 'Last name not null and max length 50').notEmpty().isLength({ max: 50}),
+    body('fullName', 'Full name not null and max length 100').notEmpty().isLength({max: 100}),
+    body('mobileOne', 'Mobile one is not null and max length 13').notEmpty().isLength({max: 13}),
+    body('mobileTwo', 'Mobile two is not null and max length 13').notEmpty().isLength({max: 13}),
+    body('emergencyMobile', 'Emergency mobile is not null and max length 13').notEmpty().isLength({max: 13}),
+    body('officeEmail', 'Office mail').isEmail().notEmpty().isLength({max: 100}),
+    body('personalEmail', 'Personal mail is not null').isEmail().notEmpty().isLength({max: 100}),
+    body('empImage', 'empImage').isEmpty(),
+    body('empSignature', 'empSignature').isEmpty(),
+    body('nationalId', 'National Id').isEmpty().isLength({max: 13}),
+  ]
+
+}
